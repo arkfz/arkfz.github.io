7d8d2c5e566d7e6ba1b09cf32cd74295489d7e63
Arkadiusz Fuz <arkadiusz.fuz@gmail.com>
2019-03-05 19:14:05 +0100

    Zadanie 4

--- zadania-domowe/js_zadanie_3/js/main.js
+++ zadania-domowe/js_zadanie_3/js/main.js
@@ -1,18 +1,16 @@
-function tablicaDzialania() {
-    let tablica = [1, 2, 3, 4, 5, 6];
+const tablica = [1, 2, 3, 4, 5, 6];
+
+function tablicaDzialania(tablica) {
     let suma = 0;
     let iloczyn = 1;
-    for (
-        let i = 0; i < tablica.length; i++) {
+    for (let i = 0; i < tablica.length; i++) {
         suma += tablica[i]
     };
-    for (
-        let i = 0; i < tablica.length; i++) {
+    for (let i = 0; i < tablica.length; i++) {
         iloczyn = iloczyn * tablica[i]
-        
     };
     console.log(suma);
     console.log(iloczyn);
 }
 
-tablicaDzialania();
+tablicaDzialania(tablica);
