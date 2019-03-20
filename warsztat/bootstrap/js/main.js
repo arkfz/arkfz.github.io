8d05295242a639d146b59453234cc15080a7cab0
Arkadiusz Fuz <arkadiusz.fuz@gmail.com>
2019-03-20 03:31:14 +0100

    Warszat Bootstrap - dokonczenie

---
+++ warsztat/bootstrap/js/main.js
@@ -0,0 +1,14 @@
+const nav = $('#nav');
+const widoczne = $('.navbar-nav .nav-item'); // Zmiana Hover
+
+$(window).on('scroll', function() {
+    if($(window).scrollTop() > 50) { //50 - wartosc pikseli przerolowania
+        nav.addClass('scroll');   
+        widoczne.addClass('zmianaHover'); // Zmiana Hover
+    } else {
+        nav.removeClass('scroll'); 
+        widoczne.removeClass('zmianaHover'); // Zmiana Hover
+    }
+});
+
+// Mozna nie na sztywno 50, tylko zrobic to dynamicznie po wysokosci sekcji i gdzie sie znajduje scroll.
---
+++ warsztat/gulp-sass/gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'));
+});
+
+
+gulp.task('watch', function () {
+    gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
+}); 
+
+// Po bledach watch sie zatrzymuje i trzeba ponownie go uruchomic. gulp watch
+// Aby wyjsc z procesu trzeba kliknac CTRL 
