9e2fff4da012d0cb1ce40b664219d3a306e0ba8b
Arkadiusz Fuz <arkadiusz.fuz@gmail.com>
2019-06-05 22:18:25 +0200

    Menu Added

--- portfolio/app/js/main.js
+++ portfolio/app/js/main.js
@@ -1,2 +1,28 @@
-// PARTICLES
+// 1. Particles
+// 2. Menu
+
+
+/*------------------------------------------------------------------------------
+1. PARTICLES
+------------------------------------------------------------------------------*/
+
 particlesJS.load("hero_particles", "particles.json");
+
+
+/*------------------------------------------------------------------------------
+2. MENU
+------------------------------------------------------------------------------*/
+
+const panel = document.querySelector("#menu-panel");
+const closeMenu = document.getElementById("close-menu");
+
+document.querySelector("#open-menu").addEventListener("click", function () {
+
+    panel.classList.add("show-menu");
+
+});
+
+document.querySelector('#close-menu').addEventListener('click', function(){
+
+    panel.classList.remove('show-menu');
+});
