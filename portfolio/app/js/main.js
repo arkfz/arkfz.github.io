737e6b9b61696f4cc0d546530a93d369bf49c72b
Arkadiusz Fuz <arkadiusz.fuz@gmail.com>
2019-06-08 22:16:50 +0200

    Added menu and scroll setts

--- portfolio/app/js/main.js
+++ portfolio/app/js/main.js
@@ -13,16 +13,22 @@ particlesJS.load("hero_particles", "particles.json");
 2. MENU
 ------------------------------------------------------------------------------*/
 
+const openMenu = document.querySelector("#open-menu");
 const panel = document.querySelector("#menu-panel");
-const closeMenu = document.getElementById("close-menu");
+const closeMenu = document.getElementsByClassName("close-menu");
 
-document.querySelector("#open-menu").addEventListener("click", function () {
+openMenu.addEventListener("click", function () {
 
     panel.classList.add("show-menu");
-
 });
 
-document.querySelector('#close-menu').addEventListener('click', function(){
-
+// Fast Solution. Check later for better...
+function close() {
     panel.classList.remove('show-menu');
-});
+}
+
+closeMenu[0].onclick = function() {close()};
+closeMenu[1].onclick = function() {close()};
+closeMenu[2].onclick = function() {close()};
+closeMenu[3].onclick = function() {close()};
+
