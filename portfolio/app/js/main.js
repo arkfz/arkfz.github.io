7778978eb6c27344c0879228ff3251e089922834
Arkadiusz Fuz <arkadiusz.fuz@gmail.com>
2019-06-09 17:27:50 +0200

    Changed Menu Concept due ScrollY Problem

--- portfolio/app/js/main.js
+++ portfolio/app/js/main.js
@@ -2,33 +2,57 @@
 // 2. Menu
 
 
-/*------------------------------------------------------------------------------
-1. PARTICLES
-------------------------------------------------------------------------------*/
+//------------------------------------------------------------------------------
+// 1. PARTICLES
+//------------------------------------------------------------------------------
 
 particlesJS.load("hero_particles", "particles.json");
 
 
-/*------------------------------------------------------------------------------
-2. MENU
-------------------------------------------------------------------------------*/
+//------------------------------------------------------------------------------
+// 2. MENU
+//------------------------------------------------------------------------------
 
-const openMenu = document.querySelector("#open-menu");
-const panel = document.querySelector("#menu-panel");
+// Open Menu
+// Fast Solution. Check later for better...
+const openMenu = document.getElementsByClassName("open-menu");
 const closeMenu = document.getElementsByClassName("close-menu");
+const panel = document.querySelector("#menu-panel");
 
-openMenu.addEventListener("click", function () {
+function openM() {
 
     panel.classList.add("show-menu");
-});
-
-// Fast Solution. Check later for better...
-function close() {
-    panel.classList.remove('show-menu');
 }
 
-closeMenu[0].onclick = function() {close()};
-closeMenu[1].onclick = function() {close()};
-closeMenu[2].onclick = function() {close()};
-closeMenu[3].onclick = function() {close()};
+openMenu[0].onclick = function() {openM()};
+openMenu[1].onclick = function() {openM()};
+openMenu[2].onclick = function() {openM()};
+openMenu[3].onclick = function() {openM()};
 
+// Close Menu
+function closeM() {
+
+    panel.classList.remove("show-menu");
+}
+
+closeMenu[0].onclick = function() {closeM()};
+closeMenu[1].onclick = function() {closeM()};
+closeMenu[2].onclick = function() {closeM()};
+closeMenu[3].onclick = function() {closeM()};
+
+
+//------------------------------------------------------------------------------
+// Change Hamburger Color - First Assumption.
+// ScrollY doesn't work due CSS Settings?
+// Check in the future.
+
+// let scrollPosition = window.scrollY;
+// window.addEventListener('scroll', function() {
+//     scrollPosition = window.scrollY;
+//     if (scrollPosition >= 30) {
+//         openMenu.classList.add('darker');
+//     } else {
+//         openMenu.classList.remove('darker');
+//     }
+// });
+//------------------------------------------------------------------------------
